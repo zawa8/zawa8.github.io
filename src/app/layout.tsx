c4192de43bf55502f84d93270dfc65d06e9b01eb
viml kumar <vimalkg@gmail.com>
2025-04-08 16:03:42 +0530

    zawa8.vercel.app mck82.vercel.app fb.com/ztrabc x.com/etphor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body> {children} </body>
     </html>
   );
